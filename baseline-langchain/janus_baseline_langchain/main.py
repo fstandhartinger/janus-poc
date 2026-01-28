466527ddc444c2b40a71002174507ee8c9baaf07
fstandhartinger <stanzy@gmx.de>
2026-01-28 11:43:51 +0100

    Avoid structlog format crashes in langchain

--- baseline-langchain/janus_baseline_langchain/main.py
+++ baseline-langchain/janus_baseline_langchain/main.py
@@ -85,7 +85,6 @@ structlog.configure(
         structlog.stdlib.filter_by_level,
         structlog.stdlib.add_logger_name,
         structlog.stdlib.add_log_level,
-        structlog.stdlib.PositionalArgumentsFormatter(),
         structlog.processors.TimeStamper(fmt="iso"),
         structlog.processors.StackInfoRenderer(),
         structlog.processors.format_exc_info,
